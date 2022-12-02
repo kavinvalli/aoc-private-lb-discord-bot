f78c7a245d795267695c3242572f6c7ceea1846e
Kavin Valli <mail@kavin.me>
2022-12-02 21:37:02 +0530

    feat: complete

---
+++ src/index.ts
@@ -0,0 +1,128 @@
+import {
+  ChatInputCommandInteraction,
+  Client,
+  Collection,
+  EmbedBuilder,
+  Events,
+  GatewayIntentBits,
+  REST,
+  Routes,
+  SlashCommandBuilder,
+} from 'discord.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+type Command = {
+  data: SlashCommandBuilder;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+};
+
+class DClient extends Client {
+  public commands: Collection<unknown, Command> = new Collection();
+}
+
+const client = new DClient({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.commands = new Collection();
+
+const command = {
+  data: new SlashCommandBuilder()
+    .setName('lb')
+    .setDescription('Shows private leaderboard of techCircuit'),
+  async execute(interaction: ChatInputCommandInteraction) {
+    try {
+      const data: {
+        members: Record<string, { name: string; local_score: number }>;
+      } = await (
+        await fetch(
+          `https://adventofcode.com/2022/leaderboard/private/view/${process.env.PRIVATE_LEADERBOARD_ID}.json`,
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              Cookie: 'session=' + process.env.SESSION,
+            },
+          }
+        )
+      ).json();
+      console.log(data);
+      let members: {
+        name: string;
+        local_score: number;
+      }[] = [];
+      for (const key in data.members) {
+        members.push(data.members[key]);
+      }
+      members.sort((a, b) => b.local_score - a.local_score);
+      members = members.slice(0, 10);
+      console.log(members.map(member => member.local_score));
+      const replyEmbed = new EmbedBuilder()
+        .setTitle("techCircuit's AOC Leaderboard")
+        .setDescription(
+          `Want to view more than the top 10 users? [**Click here**](https://adventofcode.com/2022/leaderboard/private/view/${process.env.PRIVATE_LEADERBOARD_ID})\n(you need to be logged in and have joined the leaderboard to view the above link)` +
+            '\n\n' +
+            members
+              .map(
+                (member, index) =>
+                  `**${index + 1}. ${member.name} - ${member.local_score}**`
+              )
+              .join('\n')
+        );
+      interaction.reply({ embeds: [replyEmbed] });
+    } catch (error) {
+      console.error(error);
+      interaction.reply('Something went wrong');
+    }
+  },
+};
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = (interaction.client as DClient).commands.get(
+    interaction.commandName
+  );
+
+  if (!command) {
+    console.error(`No command matching`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply('Something went wrong...');
+  }
+});
+
+client.commands.set('lb', command);
+
+const rest = new REST({ version: '10' }).setToken(
+  process.env.DISCORD_BOT_TOKEN ?? ''
+);
+
+const commands = client.commands;
+
+(async () => {
+  try {
+    console.log(`Started refreshing application (/) commands.`);
+
+    console.log(commands.map(command => command.data.toJSON()));
+
+    await rest.put(
+      Routes.applicationCommands(process.env.DISCORD_CLIENT_ID ?? ''),
+      { body: commands.map(command => command.data.toJSON()) }
+    );
+    console.log('Ho gaya shayad');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+client.login(process.env.DISCORD_BOT_TOKEN);
